eb106f0996ad0afd62cccf0ada05cd408596e192
Matt Mencel <mr-mencel@wiu.edu>
2014-07-28 17:40:48 -0500

    experimenting

--- test2.rb
+++ test2.rb
@@ -1,50 +1,98 @@
 require 'rubygems'
 require 'ap' # awesome_print gem
 require 'base64'
+require 'base64url'
 require 'hmac-sha2' # ruby-hmac gem
 require 'restclient' # rest-client gem
+require 'openssl'
+require 'open-uri'
 require 'launchy'
 
 #vars
-$hostname = 'wiutest.desire2learn.com'
-$user_id = 'sJiaLZAl41bHfa_fnyfyAp'
-$user_key = '3MznLMWrEtGU0XJddnj15W'
-$app_id = 'ysuBVIqmRDaJpnVEOb8Ylg'
-$app_key = 't3Rro3P91i_U1let5vd8Wg'
+hostname = 'wiutest.desire2learn.com'
+
+# User ID = api-user
+# User PW = see keepass
+
+user_id = 'rV5wdZwBbnT4prgaLlqx05'
+user_key = 'xVee4KJhixBCQphf-3Wuf9'
+app_id = 'ysuBVIqmRDaJpnVEOb8Ylg'
+app_key = 't3Rro3P91i_U1let5vd8Wg'
 path = '/d2l/api/lp/1.0/users/whoami'
+get_key_api = '/d2l/auth/api/token'
 httpmethod = 'GET'
-$timestamp = Time.now.to_i #current hour
+timestamp = Time.now.to_i #current hour
 
 def build_authenticated_uri_query_string(signature, timestamp)
   "?x_a=#{$app_id}&x_b=#{$user_id}&x_c=#{get_base64_hash_string($app_key, signature)}&x_d=#{get_base64_hash_string($user_key, signature)}&x_t=#{$timestamp}"
 end
 
 def authenticate_uri(path, httpmethod)
-  get_query_string($path, httpmethod)
+  get_query_string(path, httpmethod)
 end
 
 def format_signature(path, httpmethod, timestamp)
-  "#{httpmethod.upcase}&#{$path.downcase}&#{$timestamp}"
+  "#{httpmethod.upcase}&#{path.downcase}&#{timestamp}"
 end
 
 def get_base64_hash_string(key, signature)
   sha256_string = HMAC::SHA256.hexdigest(signature, key)
   sha256_string2 = HMAC::SHA256.hexdigest(key, signature)
-  ap "sig first:  #{sha256_string}" 
+  ap "sig first:  #{sha256_string}"
   ap "key first:  #{sha256_string2}"
   urlsafe_base64_string = Base64.urlsafe_encode64(sha256_string2).gsub('=', '')
 end
 
 def get_query_string(path, httpmethod)
-  signature = format_signature($path, httpmethod, $timestamp)
+  signature = format_signature(path, httpmethod, $timestamp)
   build_authenticated_uri_query_string(signature, $timestamp)
 end
 
-the_uri = "https://#{$hostname}#{$path}#{authenticate_uri($path, httpmethod)}"
+# Take the key used to create the signature and produce a set of key-bytes by
+# UTF8-encoding the key.
+key_bytes = app_key.encode('UTF-8')
+puts key_bytes
 
-puts "THE_URI:  #{the_uri}"
+# Take the base-string for the signature and produce a set of base-string-bytes
+# by UTF8-encoding the base-string.
+base_str_bytes = "https://#{hostname}/d2l/api/versions".encode('UTF-8')
+puts base_str_bytes
 
-RestClient.get(the_uri){ |response, request, result, &block|
+# Produce the HMAC-SHA256 signature hash by using the key-bytes and
+# base-string-bytes as input parameters.
+hash = OpenSSL::HMAC.digest('sha256', key_bytes, base_str_bytes)
+sig_hash =  Base64.encode64(hash)
+ap sig_hash
+
+# Take the signature hash and produce a set of signature-bytes by base64url
+# encoding the hash (see RFC 4648; no ‘=’ padding used, ‘+’ and ‘/’ replaced
+# with ‘-‘ and ‘_’ respectively).
+sig_bytes = Base64URL.encode(hash)
+ap sig_bytes
+
+# Pass these generated signature-bytes in all the query parameters where you’re
+# expected to provide a signature.
+x_target = "https://#{hostname}/d2l/api/versions"
+auth_uri = "https://#{hostname}/d2l/auth/api/token?x_a=#{app_id}&x_b=#{sig_bytes}&x_target=#{URI.escape(x_target)}"
+puts auth_uri
+
+unix_ts = Time.now.to_i
+#the_uri = "/d2l/api/lp/1.2/users/whoami"
+the_uri = "/d2l/api/lp/1.2/enrollments/users/47892/orgUnits/"
+app_str = "GET&#{the_uri}&#{unix_ts}"
+hash = OpenSSL::HMAC.digest('sha256', key_bytes, app_str)
+app_hash =  Base64.encode64(hash)
+app_bytes = Base64URL.encode(app_hash)
+ap app_bytes
+
+hash = OpenSSL::HMAC.digest('sha256', user_key.encode('UTF-8'), app_str)
+user_hash = Base64.encode64(hash)
+user_bytes = Base64URL.encode(user_hash)
+ap user_bytes
+version_uri = "https://#{hostname}#{the_uri}?x_a=#{app_id}&x_b=#{user_id}&x_c=#{app_bytes}&x_d=#{user_bytes}&x_t=#{unix_ts}"
+puts version_uri
+
+RestClient.get(version_uri){ |response, request, result, &block|
   case response.code
   when 200
     p 'It worked !'
@@ -52,11 +100,14 @@ RestClient.get(the_uri){ |response, request, result, &block|
   when 423
     fail SomeCustomExceptionIfYouWant
   when 403
-    Launchy.open(the_uri)
+    #Launchy.open(the_uri)
     ap response
   else
     response.return!(request, result, &block)
   end
+
+  ap response.return!(request, result, &block)
 }
+
 
 exit
