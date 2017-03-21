129933c01499b131fb1d59c10610c8b2bbe09cfe
Andrew Kulpa <AJ-Kulpa@wiu.edu>
2017-03-21 16:21:18 -0500

    added news functionalities; required grades.rb and news.rb in d2l_sdk.rb

--- lib/d2l_sdk.rb
+++ lib/d2l_sdk.rb
@@ -1,7 +1,7 @@
-require_relative 'd2l_sdk/setup_versions'
+require_relative 'd2l_sdk/setup_versions' # Checked
 require_relative 'd2l_sdk/course_template' # Checked
 require_relative 'd2l_sdk/course' # Checked
-require_relative 'd2l_sdk/datahub'
+require_relative 'd2l_sdk/datahub' # Checked
 require_relative 'd2l_sdk/enroll' # Checked
 require_relative 'd2l_sdk/group' # Checked
 require_relative 'd2l_sdk/org_unit' # Checked
@@ -10,8 +10,9 @@ require_relative 'd2l_sdk/semester' # Checked
 require_relative 'd2l_sdk/user' # Checked
 require_relative 'd2l_sdk/config_variables' # Checked
 require_relative 'd2l_sdk/demographics' # Checked
-require_relative 'd2l_sdk/logging'
-require_relative 'd2l_sdk/course_content'
-
+require_relative 'd2l_sdk/logging' # Checked
+require_relative 'd2l_sdk/course_content' # Checked
+require_relative 'd2l_sdk/grades' # Checked -- added 3/21
+require_relative 'd2l_sdk/news' # Checked -- added 3/21
 
 puts "d2l_sdk loaded"
--- lib/d2l_sdk/news.rb
+++ lib/d2l_sdk/news.rb
@@ -4,6 +4,19 @@ require 'json-schema'
 # News:##########
 #################
 
+# REVIEW: Delete a particular news item from an org unit.
+def delete_news_item(org_unit_id, news_item_id)
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/#{news_item_id}"
+  _delete(path)
+end
+
+# REVIEW: Delete an attachment from an org unit’s news item.
+def delete_news_item_attachment(org_unit_id, news_item_id, file_id)
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/#{news_item_id}/attachments/#{file_id}"
+  _delete(path)
+end
+
+# REVIEW: Fetch the feed for the current user context.
 # if since not specified, only includes most 'recent' feed items
 # if since specified but until is not, all items since 'since' are fetched
 # if since and until are specified, all items between these two dates are fetched
@@ -17,4 +30,79 @@ def get_current_user_feed(since = "", _until = "")
     path += "&until=#{_until}" if _until != ""
   end
   _get(path)
+end
+
+# REVIEW: Retrieve a list of news items for an org unit.
+def get_org_unit_news_items(org_unit_id, since = "")
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/"
+  path += "?since=#{since}" if since != ""
+  _get(path)
+end
+
+# REVIEW: Retrieve data blocks containing the properties of deleted news items.
+def get_deleted_news(org_unit_id, global = nil)
+  # GET /d2l/api/le/(version)/(orgUnitId)/news/deleted/
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/deleted/"
+  path += "?global=#{global}" unless global.nil?
+  _get(path)
+end
+
+# REVIEW: Retrieve a particular news item for an org unit.
+def get_org_unit_news_item(org_unit_id, news_item_id)
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/#{news_item_id}"
+  _get(path)
+end
+
+# REVIEW: Retrieve an attachment for an org unit’s news item.
+def get_news_item_attachment(org_unit_id, news_item_id, file_id)
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/#{news_item_id}/attachments/#{file_id}"
+  _get(path)
+end
+
+
+# TODO: Create a news item for an org unit.
+# INPUT: multipart/mixed POST body
+#   part 1: news item data JSON
+#   part 2: attachments
+def create_news_item(org_unit_id, news_item_data, attachments = [])
+  # POST /d2l/api/le/(version)/(orgUnitId)/news/
+end
+
+# NOTE: UNSTABLE!!!
+# REVIEW: Restore a particular news item by its news_item_id
+def restore_news_item(org_unit_id, news_item_id)
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/deleted/#{news_item_id}/restore"
+  _post(path, {})
+end
+
+# TODO: Add an attachment to a news item for an org unit.
+# INPUT: Use a multipart/form-data POST body to provide the attachment data to
+#        add to the news item, with the part’s Content-Disposition header’s
+#        name field set to “file”.
+def add_news_item_attachment(org_unit_id, news_item_id, attachment_data)
+  # POST /d2l/api/le/(version)/(orgUnitId)/news/(newsItemId)/attachments/
+end
+
+# REVIEW: Dismiss (hide) a news item for an org unit.
+def hide_news_item(org_unit_id, news_item_id)
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/#{news_item_id}/dismiss"
+  _post(path, {})
+end
+
+# REVIEW: Publish a draft news item for an org unit.
+def publish_draft_news_item(org_unit_id, news_item_id)
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/#{news_item_id}/publish"
+  _post(path, {})
+end
+
+# REVIEW: Restore (unhide) a news item for an org unit.
+def unhide_news_item(org_unit_id, news_item_id)
+  path = "/d2l/api/le/#{$le_ver}/#{org_unit_id}/news/#{news_item_id}/restore"
+  _post(path, {})
+end
+
+# TODO: Update a news item for an org unit.
+# INPUT: JSON Parameter of type NewsItemData (News.NewsItemData)
+def update_news_item(org_unit_id, news_item_id, news_item_data)
+  # PUT /d2l/api/le/(version)/(orgUnitId)/news/(newsItemId)
 end
